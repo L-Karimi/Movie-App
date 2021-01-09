cb6dc4cb3ba5cfc3ea3e08e303b50f570173d8ef
luciahroyalty101 <luciahkei101@gmail.com>
2021-01-10 01:34:44 +0300

    modified ids.xml

--- app/src/main/java/com/moringaschool/wallview/MoviesActivity.java
+++ app/src/main/java/com/moringaschool/wallview/MoviesActivity.java
@@ -1,4 +1,53 @@
 package com.moringaschool.wallview;
 
-public class MoviesActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MoviesActivity extends AppCompatActivity {
+    @BindView(R.id.locationEdit)
+    TextView mLocationTextView;
+    @BindView(R.id.listView)
+    ListView mListView;
+
+    private String[] movies = new String[] {"Insidious", "Equalizer",
+            "War Room", "Bay Watch", "Lucy", "Love and Lust",
+            "The mechanic resurrection", "Transporter", "Soul", "Avengers",
+            "Shirley", "Wonder Woman", "Roma",
+            "Get Out", "Moonlight"};
+    private String[] genres = new String[] {"Fantacy", "WAr", "Romance",
+            "Action", "Horror", "Drama", "Comedy", "Thriller",  };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        ButterKnife.bind(this);
+
+        MoviesArrayAdapter adapter = new MoviesArrayAdapter(this, android.R.layout.simple_list_item_1, movies, genres);
+        mListView.setAdapter((ListAdapter) adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(MoviesActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mLocationTextView.setText("Here are all the movies available : " );
+    }
 }
---
+++ app/src/main/java/com/moringaschool/wallview/MoviesArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.moringaschool.wallview;
+
+public class MoviesArrayAdapter {
+    public MoviesArrayAdapter(MoviesActivity moviesActivity, int simple_list_item_1, String[] movies, String[] genres) {
+
+
+    }
+}

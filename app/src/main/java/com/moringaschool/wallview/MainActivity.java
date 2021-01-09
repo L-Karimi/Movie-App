b50d952ec2c7e6b5d8493bc6a26afa62888bb1f3
luciahroyalty101 <luciahkei101@gmail.com>
2021-01-10 00:58:06 +0300

    modify build.gradle

--- app/src/main/java/com/moringaschool/wallview/MainActivity.java
+++ app/src/main/java/com/moringaschool/wallview/MainActivity.java
@@ -2,13 +2,37 @@ package com.moringaschool.wallview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.MoviesButton) Button mFindMoviesButton;
+    @BindView(R.id.LocationEdit) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindMoviesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindMoviesButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
